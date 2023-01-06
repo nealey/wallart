f9b51216ba5715d6f821d9ecfe4a78b79b702227
Neale Pickett <neale@woozle.org>
2023-01-06 10:35:31 -0700

    Add generic network server info

---
+++ network-server.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+ * Define these to fetch from a wallart-server
+ *
+ * https://git.woozle.org/neale/wallart-server
+ */
+//#define ART_HOSTNAME "wallart.your.site"
+#define ART_PORT 443
+#define ART_PATH "/wallart/wallart.bin"
